9ca9ff87f954840fb8ffdb81fbdbe768f18d7831
farazhashmi9000 <farazhashmi9000@hotmail.com>
2024-11-10 23:01:48 +0500

    Skills Model Added To The Project Model Layer;

--- myApp/models.py
+++ myApp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class skill(models.Model):
+    language = models.CharField(max_length=255)
+    skills = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.language
+    
+    
